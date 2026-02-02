fc421f1fc11860449d637c6111224fc6eeebc4f2
Christopher Kastner <245452900+MisakiGames@users.noreply.github.com>
2026-02-02 19:28:19 +0100

    started map in wrong branch oops

--- EngineWithPhysicsB2D/src/SpriteAnimationRenderComponent.h
+++ EngineWithPhysicsB2D/src/SpriteAnimationRenderComponent.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include "IRenderComponent.hpp"
-#include "SpriteRenderComponent.hpp"
+
 
 #include <SFML/Graphics.hpp>
 #include <string>
+#include <map>
 namespace mmt_gd
 {
-class SpriteAnimationRenderComponent : public SpriteRenderComponent
+class SpriteAnimationRenderComponent : public IRenderComponent
 {
 public:
     using Ptr = std::shared_ptr<SpriteAnimationRenderComponent>;
@@ -27,6 +28,17 @@ public:
 
     void draw() override;
 
+        sf::Sprite& getSprite()
+    {
+        return m_sprite;
+    }
+
+    void setTextureRect(const sf::IntRect& rect)
+    {
+        m_textureRect    = rect;
+        m_hasTextureRect = true;
+    }
+
 private:
     enum AnimationDirection
     {
@@ -37,8 +49,22 @@ private:
     };
 
 private:
+    std::string        m_textureFile;
+    sf::Texture        m_texture;
+    sf::Sprite         m_sprite;
+    std::string        m_layerName;
+    sf::IntRect        m_textureRect;
+    bool               m_hasTextureRect = false;
     float              m_gameTime = 0;
     sf::Vector2f       m_frameCount{}, m_lastFramePos;
     AnimationDirection m_direction;
 };
 } // namespace mmt_gd
+
+enum AnimationState
+{
+    Idle,
+    Walk,
+    Dead,
+    Dash
+};
