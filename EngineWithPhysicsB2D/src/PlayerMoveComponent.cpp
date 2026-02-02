51d49bb4ac43c7c9070e360102af0445aa8736ba
Christopher Kastner <245452900+MisakiGames@users.noreply.github.com>
2026-02-02 15:39:04 +0100

    Doesnt see all arguments after merge

--- EngineWithPhysicsB2D/src/PlayerMoveComponent.cpp
+++ EngineWithPhysicsB2D/src/PlayerMoveComponent.cpp
@@ -10,88 +10,88 @@
 
 namespace mmt_gd
 {
-	PlayerMoveComponent::PlayerMoveComponent(GameObject& gameObject,
-		RigidBodyComponent& rigidBody,
-		DeadComponent& deadComponent,
-		int                 playerIndex) :
-		IComponent(gameObject),
-		m_playerIndex(playerIndex),
-		m_rigidBody(rigidBody),
-		m_deadComponent(deadComponent)
-	{
-	}
+PlayerMoveComponent::PlayerMoveComponent(GameObject&         gameObject,
+                                         RigidBodyComponent& rigidBody,
+                                         DeadComponent&      deadComponent,
+                                         int                 playerIndex) :
+IComponent(gameObject),
+m_playerIndex(playerIndex),
+m_rigidBody(rigidBody),
+m_deadComponent(deadComponent)
+{
+}
 
-	bool PlayerMoveComponent::init()
-	{
-		m_rigidBody.getB2Body()->SetLinearDamping(2.F);
+bool PlayerMoveComponent::init()
+{
+    m_rigidBody.getB2Body()->SetLinearDamping(2.F);
 
-		return true;
-	}
+    return true;
+}
 
-	void PlayerMoveComponent::update(const float deltaTime)
-	{
-		if (m_deadComponent.isDead())
-			return;
+void PlayerMoveComponent::update(const float deltaTime)
+{
+    if (m_deadComponent.isDead())
+        return;
 
-		auto speed = 500.f;
-		auto dashSpeedFactor = 5.f;
-		auto dashCooldownTime = 2.f;
+    auto speed            = 500.f;
+    auto dashSpeedFactor  = 5.f;
+    auto dashCooldownTime = 2.f;
 
-		// Dash logic
+    // Dash logic
 
-		if (InputManager::getInstance().isKeyDown("dash", m_playerIndex) && m_canDash)
-		{
-			m_rigidBody.setVelocity(m_lastMoveDirection * dashSpeedFactor);
-			m_isDashing = true;
-			m_dashDuration += deltaTime;
-			return;
-		}
+    if (InputManager::getInstance().isKeyDown("dash", m_playerIndex) && m_canDash)
+    {
+        m_rigidBody.setVelocity(m_lastMoveDirection * dashSpeedFactor);
+        m_isDashing = true;
+        m_dashDuration += deltaTime;
+        return;
+    }
 
-		if (!m_canDash)
-		{
-			m_dashCooldown += deltaTime;
-			if (m_dashCooldown >= dashCooldownTime)
-			{
-				m_canDash = true;
-				m_dashCooldown = 0.f;
-				std::cout << "Dash Duration: " << m_dashDuration << "\n";
-				m_dashDuration = 0.f;
-			}
-		}
+    if (!m_canDash)
+    {
+        m_dashCooldown += deltaTime;
+        if (m_dashCooldown >= dashCooldownTime)
+        {
+            m_canDash      = true;
+            m_dashCooldown = 0.f;
+            std::cout << "Dash Duration: " << m_dashDuration << "\n";
+            m_dashDuration = 0.f;
+        }
+    }
 
-		if (InputManager::getInstance().isKeyReleased("dash", m_playerIndex) && m_isDashing)
-		{
-			m_isDashing = false;
-			m_canDash = false;
-			return;
-		}
+    if (InputManager::getInstance().isKeyReleased("dash", m_playerIndex) && m_isDashing)
+    {
+        m_isDashing = false;
+        m_canDash   = false;
+        return;
+    }
 
-		// Normal movement
+    // Normal movement
 
-		if (InputManager::getInstance().isKeyDown("right", m_playerIndex))
-		{
-			m_lastMoveDirection = sf::Vector2f(speed, 0.f);
-			m_rigidBody.setVelocity(m_lastMoveDirection);
-		}
-		else if (InputManager::getInstance().isKeyDown("left", m_playerIndex))
-		{
-			m_lastMoveDirection = sf::Vector2f(-speed, 0.f);
-			m_rigidBody.setVelocity(m_lastMoveDirection);
-		}
-		else if (InputManager::getInstance().isKeyDown("up", m_playerIndex))
-		{
-			m_lastMoveDirection = sf::Vector2f(0.f, -speed);
-			m_rigidBody.setVelocity(m_lastMoveDirection);
-		}
-		else if (InputManager::getInstance().isKeyDown("down", m_playerIndex))
-		{
-			m_lastMoveDirection = sf::Vector2f(0.f, speed);
-			m_rigidBody.setVelocity(m_lastMoveDirection);
-		}
-		else
-		{
-			m_rigidBody.setVelocity(sf::Vector2f(0.f, 0.f));
-			m_lastMoveDirection = sf::Vector2f(0.f, 0.f);
-		}
-	}
+    if (InputManager::getInstance().isKeyDown("right", m_playerIndex))
+    {
+        m_lastMoveDirection = sf::Vector2f(speed, 0.f);
+        m_rigidBody.setVelocity(m_lastMoveDirection);
+    }
+    else if (InputManager::getInstance().isKeyDown("left", m_playerIndex))
+    {
+        m_lastMoveDirection = sf::Vector2f(-speed, 0.f);
+        m_rigidBody.setVelocity(m_lastMoveDirection);
+    }
+    else if (InputManager::getInstance().isKeyDown("up", m_playerIndex))
+    {
+        m_lastMoveDirection = sf::Vector2f(0.f, -speed);
+        m_rigidBody.setVelocity(m_lastMoveDirection);
+    }
+    else if (InputManager::getInstance().isKeyDown("down", m_playerIndex))
+    {
+        m_lastMoveDirection = sf::Vector2f(0.f, speed);
+        m_rigidBody.setVelocity(m_lastMoveDirection);
+    }
+    else
+    {
+        m_rigidBody.setVelocity(sf::Vector2f(0.f, 0.f));
+        m_lastMoveDirection = sf::Vector2f(0.f, 0.f);
+    }
+}
 } // namespace mmt_gd
