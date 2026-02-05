da57f597c4a00723f3bc686e58c333b6961e76d3
Tim Paul <anonymous@example.com>
2026-02-05 14:15:49 +0100

    First Version of SoundComponent

--- EngineWithPhysicsB2D/src/SoundComponent.cpp
+++ EngineWithPhysicsB2D/src/SoundComponent.cpp
@@ -1,30 +1,77 @@
 #include "stdafx.h"
 
+#include "GameObject.hpp"
 #include "SoundComponent.hpp"
+#include <random>
 
 namespace mmt_gd
 {
+
+void SoundComponent::update(float deltaTime)
+{
+    sf::Vector2f pos = m_gameObject.getPosition();
+    for (auto& [name, sound] : m_sounds)
+    {
+        if (sound.getStatus() == sf::Sound::Playing)
+        {
+            sound.setPosition(pos.x, pos.y, 0.f);
+        }
+    }
+}
+
+void SoundComponent::addSound(const std::string& soundName, const std::string& filePath)
+{
+    sf::SoundBuffer buffer;
+    if (buffer.loadFromFile(filePath))
+    {
+        m_buffers[soundName] = buffer;
+        m_sounds[soundName].setBuffer(m_buffers[soundName]);
+        m_sounds[soundName].setRelativeToListener(false); 
+    }
+}
+
+void SoundComponent::addMusic(const std::string& musicName, const std::string& filePath)
+{
+    auto music = std::make_unique<sf::Music>();
+    if (music->openFromFile(filePath))
+    {
+        m_musics[musicName] = std::move(music);
+    }
+}
 
 void SoundComponent::playSound(const std::string& soundName)
 {
     auto it = m_sounds.find(soundName);
     if (it != m_sounds.end())
     {
+        // Random Pitch
+        static std::mt19937                   gen(std::random_device{}());
+        std::uniform_real_distribution<float> dist(m_minPitch, m_maxPitch);
+
+        it->second.setPitch(dist(gen));
+
+        // Positional Sound
+        sf::Vector2f pos = m_gameObject.getPosition();
+        it->second.setPosition(pos.x, pos.y, 0.f);
+        it->second.setMinDistance(m_soundDistance);
+        it->second.setAttenuation(1.0f);
+
         it->second.play();
     }
 }
 
-void SoundComponent::addSound(const std::string& soundName, const std::string filePath)
+void SoundComponent::playMusic(const std::string& musicName, bool loop)
 {
-    sf::Sound       sound;
-    sf::SoundBuffer buffer;
-
-    if (buffer.loadFromFile(filePath))
+    if (m_musics.count(musicName))
     {
-        sound.setBuffer(buffer);
-        m_sounds[soundName] = sound;
+        m_musics[musicName]->setLoop(loop);
+        m_musics[musicName]->play();
     }
 }
 
+void SoundComponent::stopMusic(const std::string& musicName)
+{
+    m_musics[musicName]->stop();
+}
 
 } // namespace mmt_gd
--- EngineWithPhysicsB2D/src/SoundComponent.hpp
+++ EngineWithPhysicsB2D/src/SoundComponent.hpp
@@ -11,13 +11,29 @@ namespace mmt_gd
 class SoundComponent : public IComponent
 {
 public:
+
+public:
+    explicit SoundComponent(GameObject& gameObject) : IComponent(gameObject)
+    {
+    }
+    ~SoundComponent() = default;
+
     void playSound(const std::string& soundName);
-    void playMusic(const std::string& musicName);
-    void addSound(const std::string& soundName, const std::string filePath);
-    void addMusic(const std::string& musicName, const std::string filePath);
+    void playMusic(const std::string& musicName, bool loop);
+    void stopMusic(const std::string& musicName);
+    void addSound(const std::string& soundName, const std::string& filePath);
+    void addMusic(const std::string& musicName, const std::string& filePath);
+
+    void update(float deltaTime) override;
 
 private:
-    std::unordered_map<std::string, sf::Sound> m_sounds;
-    std::unordered_map<std::string, sf::Music> m_musics;
+    std::unordered_map<std::string, sf::SoundBuffer>            m_buffers;
+    std::unordered_map<std::string, sf::Sound>                  m_sounds;
+    std::unordered_map<std::string, std::unique_ptr<sf::Music>> m_musics;
+
+    const float m_defaultVolume = 100.0f;
+    const float m_minPitch      = 0.9f;
+    const float m_maxPitch      = 1.1f;
+    const float m_soundDistance = 300.f;
 };
 } // namespace mmt_gd
