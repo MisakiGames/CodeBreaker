05a18d43a82f960194cc05c3ce5757e69ab5cfd8
Christopher Kastner <245452900+MisakiGames@users.noreply.github.com>
2026-02-12 11:08:45 +0100

    dash stops on collision

--- EngineWithPhysicsB2D/src/PlayerMoveComponent.cpp
+++ EngineWithPhysicsB2D/src/PlayerMoveComponent.cpp
@@ -44,22 +44,10 @@ void PlayerMoveComponent::update(const float deltaTime)
     auto dashCooldownTime = 0.f;
     if (m_dashActive)
     {
-        // Dash logic
-        if (m_isDashing && !m_canDash)
-        {
-            ResizeCollider();
-            /*     m_resized   = true;*/
-            m_isDashing = false;
-            m_damage.setDamageFactor(1);
-            for (auto sub : m_onDashEnd)
-                sub();
-        }
-
         if (InputManager::getInstance().isKeyDown("dash", m_playerIndex) && m_canDash)
         {
             if (!m_isDashing)
             {
-                ResizeCollider();
                 for (auto sub : m_onDash)
                     sub();
             }
@@ -75,7 +63,7 @@ void PlayerMoveComponent::update(const float deltaTime)
         if (!m_canDash)
         {
             m_dashCooldown += deltaTime;
-            if (m_dashCooldown >= dashCooldownTime)
+            if (m_dashCooldown >= dashCooldownTime && InputManager::getInstance().isKeyUp("dash", m_playerIndex))
             {
                 m_canDash      = true;
                 m_dashCooldown = 0.f;
@@ -84,9 +72,8 @@ void PlayerMoveComponent::update(const float deltaTime)
             }
         }
 
-        if (InputManager::getInstance().isKeyReleased("dash", m_playerIndex) && m_isDashing)
+        if ((InputManager::getInstance().isKeyReleased("dash", m_playerIndex) && m_isDashing) || (m_isDashing && !m_canDash))
         {
-            ResizeCollider();
             m_isDashing = false;
             m_canDash   = false;
 
@@ -138,54 +125,5 @@ void PlayerMoveComponent::DoOnMoved()
 {
     for (auto sub : m_onMoved)
         sub();
-}
-void PlayerMoveComponent::ResizeCollider()
-{
-    //auto item = m_gameObject.getComponent<PickupComponent>();
-    //if (item->getItemType() == ItemType::Size && item->getGotItemThisFrame())
-    //    return;
-
-    //float yOffsetDivisor = 2;
-    //if (m_isDashing)
-    //{
-    //    yOffsetDivisor = 1 / 1.5;
-    //}
-
-    //auto     sprite      = m_gameObject.getComponent<SpriteAnimationRenderComponent>();
-    //auto     collider    = m_gameObject.getComponent<ColliderComponent>();
-    //auto     fixture     = collider->getFixture();
-    //auto     userData    = fixture->GetUserData();
-    //b2Filter filter      = fixture->GetFilterData();
-    //float    friction    = fixture->GetFriction();
-    //float    restitution = fixture->GetRestitution();
-    //auto     player_size = sprite->getSprite().getGlobalBounds().getSize();
-    //auto     scale       = m_gameObject.getScale();
-    //if (m_isDashing)
-    //    scale = sf::Vector2f(scale.x, scale.y / 2);
-    //else
-    //    scale = sf::Vector2f(scale.x + scale.x / 2, scale.y + scale.y / 2);
-    //b2PolygonShape shape;
-    //auto           newSize = sf::Vector2f(player_size.x * scale.x, player_size.y * scale.y);
-    //const auto     size    = PhysicsManager::s2b(newSize);
-    //if (m_isDashing)
-    //    shape.SetAsBox(size.x / 2, size.y / 2, b2Vec2{size.x / 2, size.y / yOffsetDivisor}, 0);
-    //else
-    //    shape.SetAsBox(size.x / 2, size.y / 2, b2Vec2{size.x / 2, size.y / yOffsetDivisor - scale.y / 6}, 0);
-    //b2FixtureDef fixtureDef;
-    //fixtureDef.shape   = &shape;
-    //fixtureDef.density = 1.0f;
-
-    //fixtureDef.filter      = filter;
-    //fixtureDef.friction    = friction;
-    //fixtureDef.restitution = restitution;
-    //fixtureDef.userData    = userData;
-
-    //// 4. Swap fixtures
-    //collider->getBody().getB2Body()->DestroyFixture(fixture);
-    //fixture = collider->getBody().getB2Body()->CreateFixture(&fixtureDef);
-
-    //// 5. Recalculate mass for the new size
-    //collider->getBody().getB2Body()->ResetMassData();
-    //m_resized = false;
 }
 } // namespace mmt_gd
--- EngineWithPhysicsB2D/src/PlayerMoveComponent.hpp
+++ EngineWithPhysicsB2D/src/PlayerMoveComponent.hpp
@@ -61,19 +61,18 @@ public:
     }
 
 private:
-    void                               DoOnMoved();
-    void                               ResizeCollider();
-    int                                m_playerIndex;
-    DeadComponent&                     m_deadComponent;
-    RigidBodyComponent&                m_rigidBody;
-    sf::Vector2f                       m_lastMoveDirection;
+    void                DoOnMoved();
+    int                 m_playerIndex;
+    DeadComponent&      m_deadComponent;
+    RigidBodyComponent& m_rigidBody;
+    sf::Vector2f        m_lastMoveDirection;
 
-    bool                               m_isDashing    = false;
-    bool                               m_canDash      = true;
-    bool                               m_dashActive   = true;
+    bool m_isDashing  = false;
+    bool m_canDash    = true;
+    bool m_dashActive = true;
 
-    float                              m_dashCooldown = 0.f;
-    float                              m_dashDuration = 0.f;
+    float m_dashCooldown = 0.f;
+    float m_dashDuration = 0.f;
 
     std::vector<std::function<void()>> m_onDash;
     std::vector<std::function<void()>> m_onWhileDash;
