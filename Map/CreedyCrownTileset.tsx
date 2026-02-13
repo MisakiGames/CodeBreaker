49a5049e21669c5877b0b52da257473f08c5ac0c
Christopher Kastner <245452900+MisakiGames@users.noreply.github.com>
2026-02-13 19:44:18 +0100

    actually uploading the folder this time

--- Map/CreedyCrownTileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="CreedyCrownTileset" tilewidth="16" tileheight="16" tilecount="3312" columns="24">
- <image source="Map/All_Spritesheet.png" width="384" height="2208"/>
-</tileset>
--- Map/MainGame.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="MainGame" tilewidth="32" tileheight="32" tilecount="3312" columns="24">
- <image source="../EngineWithPhysicsB2D/assets/All_Spritesheet.png" width="768" height="4416"/>
- <wangsets>
-  <wangset name="Unnamed Set" type="mixed" tile="-1">
-   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
-  </wangset>
- </wangsets>
-</tileset>
